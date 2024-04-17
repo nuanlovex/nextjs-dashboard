3611b7d037ee2e915dac4a408528120c8f95c0b4
nuan <nuanlovex@163.com>
2024-04-17 19:07:19 +0800

    第一次提交

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+// 保存应用程序中使用的字体
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin']});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+});
